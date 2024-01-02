2c6c4bf1e08cbe38489aa6336a1d3bc5a2b6a9ee
Justin Trotter <trotter.justin@gmail.com>
2024-01-02 12:27:28 -0600

    trying to get dexterious developer working

--- src/lib.rs
+++ src/lib.rs
@@ -15,9 +15,61 @@ use crate::player::PlayerPlugin;
 use crate::utils::FpsCounterPlugin;
 
 use bevy::app::App;
-#[cfg(debug_assertions)]
-use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
+// #[cfg(debug_assertions)]
+use bevy::diagnostic::FrameTimeDiagnosticsPlugin;
 use bevy::prelude::*;
+use dexterous_developer::{hot_bevy_main, InitialPlugins};
+use bevy::asset::AssetMetaCheck;
+use bevy::window::PrimaryWindow;
+use bevy::winit::WinitWindows;
+use bevy::DefaultPlugins;
+use std::io::Cursor;
+use winit::window::Icon;
+
+#[hot_bevy_main]
+pub fn bevy_main(initial_plugins: impl InitialPlugins) {
+    App::new()
+        .add_plugins(initial_plugins.initialize::<DefaultPlugins>().set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Bevy game".to_string(), // ToDo
+                // Bind to canvas included in `index.html`
+                canvas: Some("#bevy".to_owned()),
+                // The canvas size is constrained in index.html and build/web/styles.css
+                fit_canvas_to_parent: true,
+                // Tells wasm not to override default event handling, like F5 and Ctrl+R
+                prevent_default_event_handling: false,
+                ..default()
+            }),
+            ..default()
+        }))
+        .insert_resource(Msaa::Off)
+        .insert_resource(AssetMetaCheck::Never)
+        .insert_resource(ClearColor(Color::rgb(0.4, 0.4, 0.4)))
+        .add_plugins(GamePlugin)
+        .add_systems(Startup, set_window_icon)
+        .run();
+}
+
+// Sets the icon on windows and X11
+fn set_window_icon(
+    windows: NonSend<WinitWindows>,
+    primary_window: Query<Entity, With<PrimaryWindow>>,
+) {
+    let primary_entity = primary_window.single();
+    let Some(primary) = windows.get_window(primary_entity) else {
+        return;
+    };
+    let icon_buf = Cursor::new(include_bytes!(
+        "../build/macos/AppIcon.iconset/icon_256x256.png"
+    ));
+    if let Ok(image) = image::load(icon_buf, image::ImageFormat::Png) {
+        let image = image.into_rgba8();
+        let (width, height) = image.dimensions();
+        let rgba = image.into_raw();
+        let icon = Icon::from_rgba(rgba, width, height).unwrap();
+        primary.set_window_icon(Some(icon));
+    };
+}
 
 // This example game uses States to separate logic
 // See https://bevy-cheatbook.github.io/programming/states.html
@@ -46,9 +98,9 @@ impl Plugin for GamePlugin {
             FpsCounterPlugin,
         ));
 
-        #[cfg(debug_assertions)]
-        {
-            app.add_plugins((FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin::default()));
-        }
+        // #[cfg(debug_assertions)]
+        // {
+            app.add_plugins(FrameTimeDiagnosticsPlugin);
+        // }
     }
 }
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use dexterous_developer::{ReloadableElementsSetup, dexterous_developer_setup, ReloadableAppContents, ReloadableApp};
 
 pub mod fps_counter;
 
@@ -6,9 +7,18 @@ use crate::{utils::fps_counter::*, GameState};
 
 pub struct FpsCounterPlugin;
 
+
 impl Plugin for FpsCounterPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(OnEnter(GameState::Playing), setup_fps_counter)
+        app
+            .setup_reloadable_elements::<reloadable>();
+    }
+}
+
+#[dexterous_developer_setup]
+fn reloadable(app: &mut ReloadableAppContents) {
+    app
+        .add_systems(OnEnter(GameState::Playing), setup_fps_counter)
             .add_systems(
                 Update,
                 (
@@ -16,5 +26,5 @@ impl Plugin for FpsCounterPlugin {
                     fps_counter_showhide.run_if(in_state(GameState::Playing)),
                 ),
             );
-    }
 }
+
